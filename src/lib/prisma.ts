0f8e8f715687ee0eb4d97ef5d7314e4a53356eaa
miu3u <miu.blue3@gmail.com>
2025-12-30 05:07:57 +0900

    fix: import prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import type { PrismaClient as PrismaClientType } from '@/generated/prisma/client';
-import { PrismaClient } from '@/generated/prisma/client';
+import type { PrismaClient as PrismaClientType } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
